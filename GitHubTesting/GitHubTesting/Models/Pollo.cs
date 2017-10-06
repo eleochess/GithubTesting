8bfe0a4235bc376a8607d71aed2a1b72a827be97
leo <leo@gmail.com>
2017-10-06 10:32:23 -0500

    changes sprint1

---
+++ GitHubTesting/GitHubTesting/Models/Pollo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GitHubTesting.Models
+{
+    public class Pollo
+    {
+        public string nombre { get; set; }
+        public string tipo { get; set; }
+    }
+}
